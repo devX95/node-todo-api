afd7d5e09d990894ff88be59e12f139502e92022
Gibran <gibrangul@hotmail.com>
2017-12-06 01:13:24 +0500

    Fixed mongodb url with password and username

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds129926.mlab.com:29926/todoapp", {
+mongoose.connect("mongodb://gibran:gib1995@ds129926.mlab.com:29926/todoapp" || "mongodb://localhost:27017/TodoApp", {
     useMongoClient: true
 });
 
