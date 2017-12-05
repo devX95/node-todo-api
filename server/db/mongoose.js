2bcef094459879c40021d74ab482ace9ec3eafdf
Gibran <gibrangul@hotmail.com>
2017-12-06 01:01:46 +0500

    Fixed mongodb url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds129926.mlab.com:29926/todoapp" || "mongodb://localhost:27017/TodoApp", {
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds129926.mlab.com:29926/todoapp", {
     useMongoClient: true
 });
 
