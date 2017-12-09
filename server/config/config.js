ffd7047e4d3925c8dd6378c0eefba439973e2103
Gibran <gibrangul@hotmail.com>
2017-12-10 01:16:44 +0500

    Update config to use JSONfile

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,17 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === "development") {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-} else if(env === "test") {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-} else if(env === "production"){
-    process.env.MONGODB_URI = "mongodb://gibran:gib1995@ds129926.mlab.com:29926/todoapp";
+if(env === "development" || env === "test") {
+    var config = require('./config.json');
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+
+// if(env === "development") {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
+// } else if(env === "test") {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
+// } else
